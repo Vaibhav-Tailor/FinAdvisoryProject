29e930cfb02164c33e60abfab4ea252c5b02eb09
Vaibhav Tailor LOQ <vaibhavttt.vt@gmail.com>
2026-04-22 10:02:23 +0530

    Blank Project

---
+++ src/main/java/com/finadvisory/FinadvisoryApplication.java
@@ -0,0 +1,14 @@
+package com.finadvisory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinadvisoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinadvisoryApplication.class, args);
+		System.out.println("Congratulations !!!! App is Started");
+	}
+
+}
---
+++ src/test/java/com/finadvisory/FinadvisoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.finadvisory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinadvisoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
